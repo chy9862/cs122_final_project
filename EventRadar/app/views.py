c6b453236f6b5dac18228fc21ad4546468bc25ca
Jonathan Manzano <jonvincruman@gmail.com>
2024-05-10 18:23:11 -0700

    Updated 'EventRadar\app\views.py': Included top docstring

--- EventRadar/app/views.py
+++ EventRadar/app/views.py
@@ -1,8 +1,22 @@
+"""Contains the routes for the application, including authentication routes.
+
+This module contains the routes for the application, including the home route and the
+login and sign-up routes. The routes are defined using the Flask Blueprint object.
+
+Attributes:
+    views: A Blueprint object that defines the routes for the application.
+
+Typical usage example:
+    return render_template("index.html", user=current_user)
+"""
+
 from flask import Blueprint, render_template, request, flash, Flask
 from flask_login import current_user
-views = Blueprint('views', __name__)
+
+# Create a Blueprint object that defines the routes for the application
+views = Blueprint("views", __name__)
 
 
-@views.route('/')
+@views.route("/")
 def home():
-    return render_template('index.html', user = current_user)
+    return render_template("index.html", user=current_user)
