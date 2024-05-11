2a37335f9c6f387bfda5c6859cbac0108993d004
Jonathan Manzano <jonvincruman@gmail.com>
2024-05-10 17:58:24 -0700

    Updated run.py: Included comments

--- EventRadar/run.py
+++ EventRadar/run.py
@@ -1,9 +1,25 @@
+"""This is the main file to run the application.
+
+This file is used to run the application. It imports the create_app function from the
+app package and creates an instance of the Flask application. The application is then
+run in debug mode.
+
+Attributes:
+    app: An instance of the Flask application.
+    create_app: A function that creates an instance of the Flask application.
+    Flask: A class that creates an instance of the Flask application.
+
+Typical usage example:
+    app.run(debug=True)
+"""
+
 from app import create_app
 from flask import Flask
 
+# Create an instance of the Flask application
 app = create_app()
 
-if __name__ == '__main__':
+# Run the application
+if __name__ == "__main__":
+    # Run the application in debug mode
     app.run(debug=True)
-
-
