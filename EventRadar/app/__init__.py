12887feb3ad8a187c7b1cdc2455f89e812bada00
Jonathan Manzano <jonvincruman@gmail.com>
2024-05-10 19:45:34 -0700

    Updated EventRader/app/__init__.py: removed unused statements and added comments/docuemntation

--- EventRadar/app/__init__.py
+++ EventRadar/app/__init__.py
@@ -18,13 +18,10 @@ Typical usage example:
     app.run(debug=True)
 """
 
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
-# from flask_migrate import Migrate
 from app.config import Config
-from os import path
 
 # Create the database instance globally
 db = SQLAlchemy()
@@ -32,37 +29,51 @@ DB_NAME = "eventradar.db"
 
 
 def create_app(config_class=Config):
-    # Initialize the app with the config from the config.py
+    """Initialize the app with the specified configuration class.
+
+    This function initializes the Flask app with the specified configuration class. It
+    sets the configuration options for the app, including the database URI and the secret
+    key for cookies. The login manager is also initialized to handle user authentication.
+    The app context is used to create the database tables if they do not exist.
+
+    Args:
+        config_class: A class representing the configuration options for the app.
+
+    Returns:
+        app: An instance of the Flask application.
+    """
     app = Flask(__name__)
     app.config.from_object(config_class)
-    # secret key for cookies that will be generated in eventRadar
+
+    # Secret key for cookies that will be generated in eventRadar
     app.config["SECRET_KEY"] = Config.SECRET_KEY
     app.config["SQLALCHEMY_DATABASE_URI"] = Config.SQLALCHEMY_DATABASE_URI
+
+    # Initialize the database
     db.init_app(app)
 
     # Initialize login manager
     login_manager = LoginManager()
-    # Login manager needs a view to redirect to when a login is required.
-    login_manager.login_view = "views.home"
+    login_manager.login_view = "views.home"  # Specify the login view
     login_manager.init_app(app)
 
-    # how a user is loaded. looks for the primary key of the user
+    # Load user function for login manager
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
 
-    # Register Blueprints
+    # Import the blueprints
     from app.auth.views import routes as routes_bp
-
-    app.register_blueprint(routes_bp, url_prefix="/auth")
-
+    from app.models.models import User
     from app.views import views as views_bp
 
+    # Register the blueprints with the app
+    app.register_blueprint(routes_bp, url_prefix="/auth")
     app.register_blueprint(views_bp, url_prefix="/")
 
-    from app.models.models import User
-
+    # Create the database tables if they do not exist
     with app.app_context():
         db.create_all()
 
+    # Return the app instance
     return app
