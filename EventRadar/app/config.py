3b19ab87a4fc194b71cbff348b41c89fefc1a11d
Hyun Woo Kim <106051666+chy9862@users.noreply.github.com>
2024-05-10 17:47:30 -0700

    Update config.py
    removed some comments that are not used

--- EventRadar/app/config.py
+++ EventRadar/app/config.py
@@ -7,17 +7,8 @@ class Config:
     SECRET_KEY = '\xcf[\xc27\xb5O^\xfa\xb0\xab\xc0\x12\x04t\xd8\xc5\x18\x14\xe0\x1f\xe4\xed7/'
     
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{DB_NAME}'
-    
-    # + os.path.join(basedir, 'eventradar.db')
-    
-    # f'sqlite:///{DB_NAME}'
-    # or \
-    #     'sqlite:///' + os.path.join(basedir, 'eventradar.db')
-    
-    # For PostgreSQL, uncomment the line below and update with your details
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost/eventradar_db'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Eventbrite API key
+    # ticketmaster API key
     api_key = "Zno3Ua2Uc6W4Am1IgDrW9osB2H6dnZct"
